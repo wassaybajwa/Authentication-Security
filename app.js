67cc5970d3b9bb6b97c813a2c5d57cdd42af6211
Wassay Bajwa <wassaybajwa@hotmail.com>
2021-07-19 01:05:47 -0400

    Committing Level-2 Authentication

---
+++ app.js
@@ -0,0 +1,86 @@
+//jshint esversion:6
+require("dotenv").config();
+const express = require("express");
+const ejs = require("ejs");
+const mongoose = require("mongoose");
+const encryption = require("mongoose-encryption");
+
+const app = express();
+
+app.use(express.urlencoded({extended: true}));
+app.use(express.static("public"));
+
+app.set("view engine", "ejs");
+
+
+mongoose.connect("mongodb://localhost:27017/userDB", {useNewUrlParser: true}); 
+
+const userSchema = new mongoose.Schema({
+    email: String,
+    password: String
+});
+
+
+userSchema.plugin(encryption, {secret: process.env.SECRET, encryptedFields: ["password"]});
+
+const User = new mongoose.model("User", userSchema);
+
+app.get("/", function(req, res){
+
+    res.render("home");
+})
+
+app.get("/login", function(req, res){
+
+    res.render("login");
+})
+
+app.get("/register", function(req, res){
+
+    res.render("register");
+})
+
+app.post("/register", function(req, res){
+
+    const newUser = new User({
+        email: req.body.username,
+        password: req.body.password
+    });
+
+    newUser.save(function(err){
+
+       if(err){
+           res.render(err);
+       }else {
+           res.render("secrets");
+       } 
+    })
+})
+
+app.post("/login", function(req, res){
+
+    const username = req.body.username;
+    const password = req.body.password;
+
+    User.findOne({email: username}, function(err, foundUser){
+
+        if(err){
+            console.log(err);
+        }else {
+            if (foundUser){
+                if(foundUser.password === password){
+                    res.render("secrets");
+                }
+            }
+        }
+    })
+})
+
+
+
+
+
+
+app.listen(3000, function(req, res){
+    console.log("Server started on port 3000")
+});
