dc94060e1ca01ef119ae3739ca2daa033c701669
Wassay Bajwa <wassaybajwa@hotmail.com>
2021-07-20 02:50:05 -0400

    express authentication with register page

--- app.js
+++ app.js
@@ -2,9 +2,13 @@
 const express = require("express");
 const ejs = require("ejs");
 const mongoose = require("mongoose");
-const bcrypt = require("bcrypt");
-const saltRounds = 10;
-
+//This is very importent to follow steps for express.session
+//make sure to keep things as follows
+//Cookies & Session
+//Step-1.
+const session = require("express-session");
+const passport = require("passport");
+const passportLocalMongoose = require("passport-local-mongoose");
 
 const app = express();
 
@@ -14,15 +18,47 @@ app.use(express.static("public"));
 
 app.set("view engine", "ejs");
 
+//Step-2.
+//place express.session initiazing object before connections after app.set
+app.use(session({ 
+    secret: "Our little secret.",
+    resave: false,
+    saveUninitialized: false
+}))
+
+//Step-3.
+//initializing passport
+app.use(passport.initialize());
+
+//Step-4
+app.use(passport.session());
+
 
 mongoose.connect("mongodb://localhost:27017/userDB", {useNewUrlParser: true}); 
-
-const userSchema = {
+mongoose.set('useCreateIndex', true); //to get rid of deprecation warning, current mongoose V5.13.3
+//Step-5
+//Change mongoose Schema
+const userSchema = new mongoose.Schema ({
     email: String,
     password: String
-};
+});
+
+//Step-6. Add plugin
+userSchema.plugin(passportLocalMongoose);
+
 
 const User = new mongoose.model("User", userSchema);
+
+//Step-7
+//Static authenticate 
+passport.use(User.createStrategy());
+
+//Step-8
+//Cookie creation and destroy passport methods
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
+
+//----End of Steps---Cookies & Session
 
 app.get("/", function(req, res){
 
@@ -34,49 +70,41 @@ app.get("/login", function(req, res){
     res.render("login");
 })
 
+app.get("/secrets", function(req, res){
+
+    if(req.isAuthenticated()){
+        res.render("secrets");
+    }else {
+        re.redirect("/login");
+    }
+})
+
 app.get("/register", function(req, res){
 
-    res.render("register");
+    res.render("register")
 })
 
 app.post("/register", function(req, res){
-
-    bcrypt.hash(req.body.password, saltRounds, function(err, hash){
-
-        const newUser = new User({
-            email: req.body.username,
-            password: hash
-        });
-        newUser.save(function(err){
-
-            if(err){
-                res.render(err);
-            }else {
-                res.render("secrets");
-            } 
-         })
-    })   
-})
-
-app.post("/login", function(req, res){
-
-    const username = req.body.username;
-    const password = req.body.password;
-
-    User.findOne({email: username}, function(err, foundUser){
+//Authenticate method
+User.register({username: req.body.username}, req.body.password, function(err, user){
 
         if(err){
             console.log(err);
-        }else {
-            if (foundUser){
-                bcrypt.compare(password, foundUser.password, function(err, result){
-                    if(result === true){
-                        res.render("secrets");
-                    }
-                })               
-            }
-        }
-    })
+            res.redirect("/register");
+        } else {
+            passport.authenticate("local")(req, res, function(){
+
+             res.redirect("/secrets");
+        }) 
+    }
+})
+})
+    
+
+
+app.post("/login", function(req, res){
+
+    
 })
 
 
