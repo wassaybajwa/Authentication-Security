ddafe63c66d14abb3c9a9ebcc6d1782212b9ab67
Wassay Bajwa <wassaybajwa@hotmail.com>
2021-07-20 03:03:02 -0400

    express authentication with login page

--- app.js
+++ app.js
@@ -104,7 +104,20 @@ User.register({username: req.body.username}, req.body.password, function(err, us
 
 app.post("/login", function(req, res){
 
-    
+   const user = new User({
+       username: req.body.username,
+       password: req.body.password
+   });
+   
+   req.login(user, function(err){
+       if(err){
+           console.log(err);
+       }else {
+           passport.authenticate("local")(req, res, function(){
+               res.redirect("secrets");
+           })
+       }
+   })
 })
 
 
